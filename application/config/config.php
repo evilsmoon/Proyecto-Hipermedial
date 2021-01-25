d44dfc99637083ae02c1096988111d30f741a827
Gabriel Morales <gabrielmorales201564@hotmail.com>
2021-01-25 08:12:16 -0500

    change congfig.php base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/proyecto-hipermedial/';
+$config['base_url'] = 'http://localhost/Proyecto-Hipermedial/';
 /*
 |--------------------------------------------------------------------------
 | Index File
