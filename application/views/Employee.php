2d7f1afc10f8b2a1661dca4999a175883bfbe6ca
Gabriel Morales <gabrielmorales201564@hotmail.com>
2021-02-08 22:29:12 -0500

    merge

--- application/views/Employee.php
+++ application/views/Employee.php
@@ -1,27 +1,4 @@
-<<<<<<< HEAD
 
-
-<?php
-=======
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Profile</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">User Profile</li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
->>>>>>> 4bd9be0e7283d9bc80f3828fe5a1221165539d33
 
     <!-- Main content -->
     <section class="content">
